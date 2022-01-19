253259fb9ca1307204e771af9ca69b60c00a59f1
Jonas Deprund <jonasdeprund@gmail.com>
2022-01-19 16:18:30 +0100

    boilerplate shader done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/orbitControls';
 
 export default class Sketch {
   constructor(options) {
@@ -18,18 +19,44 @@ export default class Sketch {
     this.camera.position.z = 1;
 
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setSize(this.width, this.height);
     this.container.appendChild(this.renderer.domElement);
 
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    this.resize();
+    this.setupResize();
     this.addObjects();
     this.render();
   }
 
-  resize() {}
+  setupResize() {
+    window.addEventListener('resize', this.resize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
+  }
 
   addObjects() {
-    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    this.geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
     this.material = new THREE.MeshNormalMaterial();
+
+    this.material = new THREE.ShaderMaterial({
+      fragmentShader: `
+        void main() {
+            gl_FragColor = vec4(1., 0, 0, 1.);
+        }
+      `,
+      vertexShader: `
+        void main(){
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+      `,
+    });
 
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.scene.add(this.mesh);
